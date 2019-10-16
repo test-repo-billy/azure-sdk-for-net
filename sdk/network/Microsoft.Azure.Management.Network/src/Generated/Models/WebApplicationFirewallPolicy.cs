c5323d6007813ebf998069965b4ce371f2e64fb5
SDK Automation <sdkautomation@microsoft.com>
2019-10-16 05:44:09 +0000

    Generated from 24557fea301ed32cee21432e84c1ee7241d45a80
     making managedRuleSets as optional

--- sdk/network/Microsoft.Azure.Management.Network/src/Generated/Models/WebApplicationFirewallPolicy.cs
+++ sdk/network/Microsoft.Azure.Management.Network/src/Generated/Models/WebApplicationFirewallPolicy.cs
@@ -51,9 +51,15 @@ namespace Microsoft.Azure.Management.Network.Models
         /// application firewall policy resource. Possible values include:
         /// 'Succeeded', 'Updating', 'Deleting', 'Failed'</param>
         /// <param name="resourceState">Resource status of the policy.</param>
+        /// <param name="managedRules">Describes the managedRules
+        /// structure</param>
+        /// <param name="httpListeners">A collection of references to
+        /// application gateway http listeners.</param>
+        /// <param name="pathBasedRules">A collection of references to
+        /// application gateway path rules.</param>
         /// <param name="etag">A unique read-only string that changes whenever
         /// the resource is updated.</param>
-        public WebApplicationFirewallPolicy(string id = default(string), string name = default(string), string type = default(string), string location = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), PolicySettings policySettings = default(PolicySettings), IList<WebApplicationFirewallCustomRule> customRules = default(IList<WebApplicationFirewallCustomRule>), IList<ApplicationGateway> applicationGateways = default(IList<ApplicationGateway>), string provisioningState = default(string), string resourceState = default(string), string etag = default(string))
+        public WebApplicationFirewallPolicy(string id = default(string), string name = default(string), string type = default(string), string location = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), PolicySettings policySettings = default(PolicySettings), IList<WebApplicationFirewallCustomRule> customRules = default(IList<WebApplicationFirewallCustomRule>), IList<ApplicationGateway> applicationGateways = default(IList<ApplicationGateway>), string provisioningState = default(string), string resourceState = default(string), ManagedRulesDefinition managedRules = default(ManagedRulesDefinition), IList<SubResource> httpListeners = default(IList<SubResource>), IList<SubResource> pathBasedRules = default(IList<SubResource>), string etag = default(string))
             : base(id, name, type, location, tags)
         {
             PolicySettings = policySettings;
@@ -61,6 +67,9 @@ namespace Microsoft.Azure.Management.Network.Models
             ApplicationGateways = applicationGateways;
             ProvisioningState = provisioningState;
             ResourceState = resourceState;
+            ManagedRules = managedRules;
+            HttpListeners = httpListeners;
+            PathBasedRules = pathBasedRules;
             Etag = etag;
             CustomInit();
         }
@@ -107,11 +116,63 @@ namespace Microsoft.Azure.Management.Network.Models
         public string ResourceState { get; private set; }
 
         /// <summary>
+        /// Gets or sets describes the managedRules structure
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.managedRules")]
+        public ManagedRulesDefinition ManagedRules { get; set; }
+
+        /// <summary>
+        /// Gets a collection of references to application gateway http
+        /// listeners.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.httpListeners")]
+        public IList<SubResource> HttpListeners { get; private set; }
+
+        /// <summary>
+        /// Gets a collection of references to application gateway path rules.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.pathBasedRules")]
+        public IList<SubResource> PathBasedRules { get; private set; }
+
+        /// <summary>
         /// Gets or sets a unique read-only string that changes whenever the
         /// resource is updated.
         /// </summary>
         [JsonProperty(PropertyName = "etag")]
         public string Etag { get; set; }
 
+        /// <summary>
+        /// Validate the object.
+        /// </summary>
+        /// <exception cref="ValidationException">
+        /// Thrown if validation fails
+        /// </exception>
+        public virtual void Validate()
+        {
+            if (PolicySettings != null)
+            {
+                PolicySettings.Validate();
+            }
+            if (CustomRules != null)
+            {
+                foreach (var element in CustomRules)
+                {
+                    if (element != null)
+                    {
+                        element.Validate();
+                    }
+                }
+            }
+            if (ApplicationGateways != null)
+            {
+                foreach (var element1 in ApplicationGateways)
+                {
+                    if (element1 != null)
+                    {
+                        element1.Validate();
+                    }
+                }
+            }
+        }
     }
 }
