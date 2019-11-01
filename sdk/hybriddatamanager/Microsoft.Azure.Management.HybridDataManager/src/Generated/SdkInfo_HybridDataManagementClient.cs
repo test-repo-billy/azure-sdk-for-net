5af6fb91f10e2f0f5d8eb192e28c085d78c95661
SDK Automation <sdkautomation@microsoft.com>
2019-11-01 06:58:49 +0000

    Generated from 27d5fcbffb59163e5aeda0e779ecd92fff5ae415
    add clear-output-folder in resource-manager readme.typescript.md to fix js sdk build failure

--- sdk/hybriddatamanager/Microsoft.Azure.Management.HybridDataManager/src/Generated/SdkInfo_HybridDataManagementClient.cs
+++ sdk/hybriddatamanager/Microsoft.Azure.Management.HybridDataManager/src/Generated/SdkInfo_HybridDataManagementClient.cs
@@ -30,16 +30,5 @@ namespace Microsoft.Azure.Management.HybridData
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/hybriddatamanager/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Users\\t-prta\\GitHub\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "463b431f324d40c08f510276509d2f851ace4a54";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
